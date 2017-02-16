442d3b6c5f0b58762189653d9e2f6ec062057798
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-16 18:51:03 -0500

    mapped all my variables

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+import { GH_TOKEN } from './token';
+
+function getData (query) {
+  return $.ajax({
+    url: `https://api.soundcloud.com/tracks/?client_id=${GH_TOKEN}`,
+    dataType: 'json',
+    //success: console.log,
+    data: {
+      q: query
+    }
+  });
+}
+
+function submitButton (event) {
+  event.preventDefault();
+  var query = $('#track-input').val();
+  getData(query).then(setData);
+
+}
+
+$('form').submit(submitButton);
+
+// var piano = "piano";
+// getData(piano);
+
+function setData (data) {
+  var result = {};
+  for (var i=0; i<data.length; i++){
+    var info = data[i];
+    result.trackTitle = info.title;
+    result.artWork = info.artwork_url;
+    result.bandName = info.user.username;
+    $('.tracks-container').append(trackTemple(result))
+  }
+  console.log(result);
+}
